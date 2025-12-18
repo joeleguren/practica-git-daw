5e8a6e030893e20f2107ec9f8e18ec7c0f2cb856
Joel Eguren <joeleguren7@gmail.com>
2025-12-18 23:33:55 +0100

    Branca feature/add-tasks

--- index.php
+++ index.php
@@ -2,6 +2,23 @@
 // Sistema de Gestió de Tasques
 // Versió 1.0
 
+function mostrarTasques($tasques) {
+    echo "=== LLISTA DE TASQUES ===\n";
+    foreach ($tasques as $id => $tasca) {
+        echo "[$id] $tasca\n";
+    }
+}
+
+// Crea un array amb almenys 3 tasques
+$tasques = [
+    "Comprar pa",
+    "Fer els deures",
+    "Netejar l'habitació"
+];
+
 echo "=== GESTOR DE TASQUES ===\n";
-echo "Benvingut al sistema!\n";
+echo "Benvingut al sistema!\n\n";
+
+// Crida a la funció mostrarTasques
+mostrarTasques($tasques);
 ?>
