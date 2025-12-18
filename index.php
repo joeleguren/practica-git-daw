310f7c00e33a37e3ea7a511daa2508f3b4f1dcbd
Joel Eguren <joeleguren7@gmail.com>
2025-12-18 23:35:31 +0100

    Branca feature/add-user-system

--- index.php
+++ index.php
@@ -2,6 +2,15 @@
 // Sistema de Gestió de Tasques
 // Versió 1.0
 
-echo "=== GESTOR DE TASQUES ===\n";
-echo "Benvingut al sistema!\n";
+function mostrarUsuari($nom) {
+    echo "=== GESTOR DE TASQUES ===\n";
+    echo "Usuari actual: $nom\n";
+    echo "Benvingut al sistema!\n";
+}
+
+// Crea una variable $usuariActual amb un nom
+$usuariActual = "Joel Eguren";
+
+// Crida a la funció mostrarUsuari
+mostrarUsuari($usuariActual);
 ?>
